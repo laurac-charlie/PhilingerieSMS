73708527f3bfc84e3c011ebb1f1b825c904cd9fb
charlie <charlie.laurac@supinfo.com>
2012-12-02 02:32:58 -0400

    Création de l'application Android d'envoi de masse de SMS pour Philingerie

---
+++ gen/com/isd360/philingerie_sms/activity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.isd360.philingerie_sms.activity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/isd360/philingerie_sms/activity/Acceuil.java
@@ -0,0 +1,13 @@
+package com.isd360.philingerie_sms.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Acceuil extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
